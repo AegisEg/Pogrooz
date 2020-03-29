60a59d2896b34342d98abaec03955f7a575a00ad
Aega <neostar1996@mail.ru>
2020-03-29 22:08:52 +0500

    header

--- client/src/Elements/Button.js
+++ client/src/Elements/Button.js
@@ -4,7 +4,7 @@ import React from 'react'
 class Button extends React.Component {
     render() {
         return (
-            <span className={`button-${this.props.type}`} style={this.props.paddingVertical ? {padding: `${this.props.paddingVertical} ${this.props.paddingHorizontal ? this.props.paddingHorizontal : '24px'}`, margin: this.props.margin ? this.props.margin : ''} : {}}>
+            <span className={`button-${this.props.type}`} style={this.props.paddingVertical ? {padding: `${this.props.paddingVertical} ${this.props.paddingHorizontal ? this.props.paddingHorizontal : '15px'}`, margin: this.props.margin ? this.props.margin : ''} : {}}>
                 {this.props.children}
             </span>
         )
