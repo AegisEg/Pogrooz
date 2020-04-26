2f6d648192174d9248367a2cf7d3f5f6c75365bc
Roman Shuvalov <pffbread@gmail.com>
2020-04-26 19:19:21 +0300

    ssl

--- server/database.js
+++ server/database.js
@@ -11,7 +11,8 @@ let db = {
       host: process.env.SQL_HOST,
       user: process.env.SQL_USER,
       password: process.env.SQL_PASSWORD,
-      database: process.env.SQL_DATABASE
+      database: process.env.SQL_DATABASE,
+      port: 3306
     },
     useNullAsDefault: true,
 };
