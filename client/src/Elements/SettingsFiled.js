46bb2ea059083bb5606acb84f4f6269283a5066d
Aega <neostar1996@mail.ru>
2020-05-11 19:04:49 +0500

    NotificationNotAdaptive

---
+++ client/src/Elements/SettingsFiled.js
@@ -0,0 +1,35 @@
+// App
+import React from "react";
+import CheckBox from "./CheckBox";
+
+class SettingsFiled extends React.Component {
+  render() {    
+    return (
+      <div className="row mx-0 user-settings">
+        <div className="filed-label">{this.props.label}</div>
+        <div className="col fields-checkbox">
+          <span className="align-middle">
+            <CheckBox
+              id={`${this.props.keyField}-mail`}
+              name={`${this.props.keyField}-mail`}
+              text="На почту"
+              wrapperlabelClass="ml-2"
+              labelClass="f-12 gray"
+            />
+          </span>
+          <span className="align-middle">
+            <CheckBox
+              id={`${this.props.keyField}-push`}
+              name={`${this.props.keyField}-push`}
+              text="Push-уведомления"
+              wrapperlabelClass="ml-2"
+              labelClass="f-12 gray"
+            />
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SettingsFiled;
