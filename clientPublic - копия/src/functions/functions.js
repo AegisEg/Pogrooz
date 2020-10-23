67ef094d744f13d3c5af07e04c67cbfea48f057d
Aega <neostar1996@mail.ru>
2020-10-23 19:35:44 +0500

    ыва

--- clientPublic - копия/src/functions/functions.js
+++
@@ -1,5 +0,0 @@
-export function setForceTitle(title) {
-  var pageTitle;
-  pageTitle = "Pogrooz | " + title;
-  document.title = pageTitle ? pageTitle : "Pogrooz";
-}
--- clientPublic/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-
-import rootReducer from './reducers'
-import ReduxThunk from 'redux-thunk'
-
-export default createStore(rootReducer, applyMiddleware(ReduxThunk))
